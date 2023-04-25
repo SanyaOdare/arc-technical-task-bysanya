c404aa886acc12c1280046352eccf9c52e918796
SanyaOdare <sanyaodare@gmail.com>
2023-04-25 13:43:26 +0100

    style navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
-// import logo from './logo.svg';
+import logo from './assets/header-logo.png';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">Arena Racing Company (ARC)</div>
+    <div className="App">
+      <img src={logo} className="App-logo" alt="logo" />
+      {/* <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header> */}
+    </div>
   );
 }
 
